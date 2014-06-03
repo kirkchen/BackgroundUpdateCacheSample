3fbf75522e9e491745fd6fa07477f0af2276fe39
Kirk Chen <rwk0119@gmail.com>
2014-06-03 16:33:27 +0800

    Integrate with redis

---
+++ BackgroundUpdateCache.Website/Extensions/StackExchangeRedisExtension.cs
@@ -0,0 +1,59 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Web;
+
+namespace BackgroundUpdateCache.Website.Extensions
+{
+    public static class StackExchangeRedisExtension
+    {
+        public static T Get<T>(this IDatabase cache, string key)
+        {
+            return Deserialize<T>(cache.StringGet(key));
+        }
+
+        public static object Get(this IDatabase cache, string key)
+        {
+            return Deserialize<object>(cache.StringGet(key));
+        }
+
+        public static void Set(this IDatabase cache, string key, object value, TimeSpan? expire = null)
+        {
+            cache.StringSet(key, Serialize(value), expire);
+        }
+
+        static byte[] Serialize(object o)
+        {
+            if (o == null)
+            {
+                return null;
+            }
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                binaryFormatter.Serialize(memoryStream, o);
+                byte[] objectDataAsStream = memoryStream.ToArray();
+                return objectDataAsStream;
+            }
+        }
+
+        static T Deserialize<T>(byte[] stream)
+        {
+            if (stream == null)
+            {
+                return default(T);
+            }
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (MemoryStream memoryStream = new MemoryStream(stream))
+            {
+                T result = (T)binaryFormatter.Deserialize(memoryStream);
+                return result;
+            }
+        }
+    }
+}
--- BackgroundUpdateCache.Website/Services/LongRunningService.cs
+++ BackgroundUpdateCache.Website/Services/LongRunningService.cs
@@ -1,4 +1,6 @@
-﻿using BackgroundUpdateCache.Website.Models;
+﻿using BackgroundUpdateCache.Website.Extensions;
+using BackgroundUpdateCache.Website.Models;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +11,41 @@ namespace BackgroundUpdateCache.Website.Services
 {
     public class LongRunningService : ILongRunningService
     {
+        public ConnectionMultiplexer RedisConnection { get; set; }
+
+        public LongRunningService(ConnectionMultiplexer connection)
+        {
+            this.RedisConnection = connection;
+        }
+
         public CacheDto<IEnumerable<int>> GetData(int min, int max)
         {
-            Thread.Sleep(5000);
+            var cache = this.RedisConnection.GetDatabase();
 
-            var r = new Random();
-            var result = new List<int>();
-
-            for (int i = 0; i < 10; i++)
+            var key = string.Format("LongRunningService.GetData.{0}.{1}", min, max);
+            var cachedData = cache.Get<CacheDto<IEnumerable<int>>>(key);
+            if (cachedData == null)
             {
-                result.Add(r.Next(min, max));
+                Thread.Sleep(5000);
+
+                var r = new Random();
+                var result = new List<int>();
+
+                for (int i = 0; i < 10; i++)
+                {
+                    result.Add(r.Next(min, max));
+                }
+
+                cachedData = new CacheDto<IEnumerable<int>>
+                {
+                    Data = result,
+                    UpdateTime = DateTime.Now
+                };
+
+                cache.Set(key, cachedData, TimeSpan.FromSeconds(30));
             }
 
-            return new CacheDto<IEnumerable<int>>
-            {
-                Data = result,
-                UpdateTime = DateTime.Now
-            };
+            return cachedData;
         }
     }
 }
